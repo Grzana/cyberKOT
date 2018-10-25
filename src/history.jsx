c23ee27da24f563a05c0107b1ecfadedc4c17dcd
Grzana <anet.cieslik@gmail.com>
2018-10-25 21:01:20 +0200

    mapping to table improved but return still not working

--- src/history.jsx
+++ src/history.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import firebase, { db } from './firebase_config.jsx';
+import firebase, {db}  from './firebase_config.jsx';
 
 class History extends React.Component {
 
     getOrders = () => { // query all documents from "Orders" collection
         let arr = [];
         return db.collection("Orders").get().then((querySnapshot) => {
-            
             querySnapshot.forEach(doc => arr.push(doc.data()));
             return arr;
-
         });
     };
 
@@ -19,29 +17,32 @@ class History extends React.Component {
             let rows = [];
             for (let i = 0; i < orders.length; i++) {
                 let columns = [];
-
-                for (let j = 0; j < 1; j++) {
-                    columns.push(orders[i].id);
+                let tags=['id', 'date', 'customer', 'user', 'status'];
+                for (let j = 0; j < tags.length; j++) {
+                    let tag = tags[j];
+                    columns.push(orders[i][tag]);
                 };
                 
-                rows.push(columns);
+                rows.push(columns); // outcome: rows: array with orders 
+                                    // where elements are arrays (named 'columns') with 5 elements -> check tags array
             };
-            console.log(rows);
-            //let rows2 = [ [1], [2] ];
-            //return rows2.map((el, i) => <tr key={i}>{el.map((data, j) => <td key={j}>{data[0]}</td>)}</tr>);
-            return rows;
+            console.log('rows: ', rows);
+            //this.setState({dataCaught: true});
+            //return rows.map((el, i) => <tr key={i}>{el.map((data, j) => <td key={j}>{data[0]}</td>)}</tr>);
+            return rows
         });
         return res;
         //this.getOrders().then(result => console.log('result: ', result));
     };
 
-    dupa = () => {
-        let dupsko = this.printTable();
-        console.log('dupa: ', dupsko);
+    test = () => {
+        let rows = this.printTable();
+        console.log('test: ', rows);
+        return rows.map((el, i) => <tr key={i}><td>{el}</td></tr>);
     };
 
     render() {
-
+        
         return <div>
             <div>
                 <h1>Historia zleceń</h1>
@@ -49,7 +50,7 @@ class History extends React.Component {
 
             <table>
                 <tbody>
-                    {this.dupa()}
+                    {this.test()}
                 </tbody>
             </table>
 
