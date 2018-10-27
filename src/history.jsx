e06699a9f95ee79c10386dc543aca498f520056c
Grzana <anet.cieslik@gmail.com>
2018-10-27 17:43:25 +0200

    table is rendered but problems with Network

--- src/history.jsx
+++ src/history.jsx
@@ -1,59 +1,74 @@
 import React from 'react';
-import firebase, {db}  from './firebase_config.jsx';
+import firebase, { db } from './firebase_config.jsx';
+
+class Loading extends React.Component {
+    render() {
+        return <div>
+            Dane są ładowane...
+        </div>
+    }
+}
+
+class Table extends React.Component {
+
+    printTable = (orders) => {
+
+        let rows = [];
+        for (let i = 0; i < orders.length; i++) {
+            let columns = [];
+            let tags = ['id', 'date', 'customer', 'user', 'status']; // these are column names
+            for (let j = 0; j < tags.length; j++) {
+                let tag = tags[j];
+                columns.push(orders[i][tag]);
+            };
+
+            rows.push(columns); // outcome: rows: array with orders 
+            // where elements are arrays (named 'columns') with 5 elements -> check tags array
+        };
+        return rows
+        //this.getOrders().then(result => console.log('result: ', result));
+    };
+
+    render() {
+        return <table>
+            <tbody>
+                {this.printTable(this.props.data).map((el, i) => <tr key={i}><td>{el}</td></tr>)}
+            </tbody>
+        </table>
+    }
+}
 
 class History extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            caught: false,
+            data: []
+        }
+    }
 
     getOrders = () => { // query all documents from "Orders" collection
         let arr = [];
         return db.collection("Orders").get().then((querySnapshot) => {
             querySnapshot.forEach(doc => arr.push(doc.data()));
+            this.setState({ caught: true, data: arr });
             return arr;
         });
     };
 
-    printTable = () => {
-        
-        let res = this.getOrders().then((orders) => {
-            let rows = [];
-            for (let i = 0; i < orders.length; i++) {
-                let columns = [];
-                let tags=['id', 'date', 'customer', 'user', 'status'];
-                for (let j = 0; j < tags.length; j++) {
-                    let tag = tags[j];
-                    columns.push(orders[i][tag]);
-                };
-                
-                rows.push(columns); // outcome: rows: array with orders 
-                                    // where elements are arrays (named 'columns') with 5 elements -> check tags array
-            };
-            console.log('rows: ', rows);
-            //this.setState({dataCaught: true});
-            //return rows.map((el, i) => <tr key={i}>{el.map((data, j) => <td key={j}>{data[0]}</td>)}</tr>);
-            return rows
-        });
-        return res;
-        //this.getOrders().then(result => console.log('result: ', result));
-    };
-
-    test = () => {
-        let rows = this.printTable();
-        console.log('test: ', rows);
-        return rows.map((el, i) => <tr key={i}><td>{el}</td></tr>);
-    };
-
     render() {
-        
+        this.getOrders();
+        let info;
+        if (!this.state.caught) {
+            this.info = <Loading />
+        } else {
+            this.info = <Table data={this.state.data} />
+        };
         return <div>
             <div>
                 <h1>Historia zleceń</h1>
             </div>
-
-            <table>
-                <tbody>
-                    {this.test()}
-                </tbody>
-            </table>
-
+                {this.info}
             <div>
                 <button>Cofnij</button>
             </div>
